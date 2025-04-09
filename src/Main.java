f945a612f49e5b67811ba0164ac5c9d346ed39ac
Dima <dapariciopereira@danielcastelao.org>
2025-04-08 18:07:57 -1200

    Remove de metodo tempscore, ya que no se utilizaba

--- src/Main.java
+++ src/Main.java
@@ -3,42 +3,49 @@ public class Main {
      * metodo para devolver la puntuacion de tenis
      */
     public static String getScore(int PuntosJugador1, int PuntosJugador2) {
-        String score = "";
-        int tempScore=0;
-        
-
+        String score=" ";
         if (isTie(PuntosJugador1, PuntosJugador2)) {
-            score = getTieScores(PuntosJugador1);
+            return getTieScores(PuntosJugador1);
         }
         else if (isAdvantageOrWin(PuntosJugador1, PuntosJugador2))
         {
-            score = getAdvantageOrWinScores(PuntosJugador1, PuntosJugador2);
+            return getAdvantageOrWinScores(PuntosJugador1, PuntosJugador2);
         }
         else
         {
-            for (int i=1; i<3; i++)
-            {
-                if (isTie(i, 1)) tempScore = PuntosJugador1;
-                else { score+="-"; tempScore = PuntosJugador2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-
+            return getRegularScores(PuntosJugador1, PuntosJugador2, score);
         }
-    return score;
+
+    }
+
+    private static String getRegularScores(int PuntosJugador1, int PuntosJugador2, String score) {
+        score = getMejorDeDos(PuntosJugador1, PuntosJugador2, score);
+        return score;
+    }
+
+    private static String getMejorDeDos(int PuntosJugador1, int PuntosJugador2, String score) {
+        int tempScore;
+        for (int i = 1; i<3; i++)
+        {
+            if (isTie(i, 1)) tempScore = PuntosJugador1;
+            else { score +="-"; tempScore = PuntosJugador2;}
+            switch(tempScore)
+            {
+                case 0:
+                    score +="Love";
+                    break;
+                case 1:
+                    score +="Fifteen";
+                    break;
+                case 2:
+                    score +="Thirty";
+                    break;
+                case 3:
+                    score +="Forty";
+                    break;
+            }
+        }
+        return score;
     }
 
     private static String getAdvantageOrWinScores(int PuntosJugador1, int PuntosJugador2) {
