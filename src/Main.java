ba8d5ce0e8866af4ba4885161ec5f93d7ae8ebb8
Dima <dapariciopereira@danielcastelao.org>
2025-04-08 17:47:48 -1200

    Hacer legibles las variables de cada jugador

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ public class Main {
     /**
      * metodo para devolver la puntuacion de tenis
      */
-    public static String getScore(int m_score1, int m_score2) {
+    public static String getScore(int PuntosJugador1, int PuntosJugador2) {
         String score = "";
         int tempScore=0;
 
-        if (m_score1 == m_score2) {
-            switch (m_score1)
+
+        if (PuntosJugador1 == PuntosJugador2) {
+            switch (PuntosJugador1)
             {
                 case 0:
                     score = "Love-All";
@@ -27,9 +28,9 @@ public class Main {
 
             }
         }
-        else if (m_score1 >=4 || m_score2 >=4)
+        else if (PuntosJugador1 >=4 || PuntosJugador2 >=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = PuntosJugador1-PuntosJugador2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -39,8 +40,8 @@ public class Main {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = PuntosJugador1;
+                else { score+="-"; tempScore = PuntosJugador2;}
                 switch(tempScore)
                 {
                     case 0:
