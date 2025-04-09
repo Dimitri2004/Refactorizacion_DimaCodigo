68da08f7007ea2e0ef43be9027a0684cda3e3829
Dima <dapariciopereira@danielcastelao.org>
2025-04-08 17:57:56 -1200

    Extraccion de metodos de victoria o ventaja para mayor limpieza y reutilización

--- src/Main.java
+++ src/Main.java
@@ -8,33 +8,11 @@ public class Main {
         
 
         if (isTie(PuntosJugador1, PuntosJugador2)) {
-            switch (PuntosJugador1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
+            score = getTieScores(PuntosJugador1);
         }
-        else if (PuntosJugador1 >=4 || PuntosJugador2 >=4)
+        else if (isAdvantageOrWin(PuntosJugador1, PuntosJugador2))
         {
-            int minusResult = PuntosJugador1-PuntosJugador2;
-            if (isTie(minusResult, 1)) score ="Advantage player1";
-            else if (isTie(minusResult, -1)) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = getAdvantageOrWinScores(PuntosJugador1, PuntosJugador2);
         }
         else
         {
@@ -61,6 +39,44 @@ public class Main {
 
         }
     return score;
+    }
+
+    private static String getAdvantageOrWinScores(int PuntosJugador1, int PuntosJugador2) {
+        String score;
+        int minusResult = PuntosJugador1 - PuntosJugador2;
+        if (isTie(minusResult, 1)) score ="Advantage player1";
+        else if (isTie(minusResult, -1)) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private static boolean isAdvantageOrWin(int PuntosJugador1, int PuntosJugador2) {
+        return PuntosJugador1 >= 4 || PuntosJugador2 >= 4;
+    }
+
+    private static String getTieScores(int PuntosJugador1) {
+        String score;
+        switch (PuntosJugador1)
+        {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
     }
 
     private static boolean isTie(int PuntosJugador1, int PuntosJugador2) {
