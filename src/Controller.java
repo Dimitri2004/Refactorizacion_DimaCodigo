46607e68a7c9ba0e1d26107ba9fbf54af31cdcf0
Dima <dapariciopereira@danielcastelao.org>
2025-04-30 14:22:57 +0200

    MVC 0.6

--- src/Controller.java
+++ src/Controller.java
@@ -2,43 +2,41 @@ import java.lang.module.ModuleDescriptor;
 
 public class Controller {
     public static void main(String[] args) {
-     /*   // Instanciamos la vista y el modelo
-        View miView = new View();
-        Model miModel = new Model();
+       // Instanciamos la vista y el modelo
+
 
         // Crear tres coches
-        miModel.crearCoche("LaFerrari", "SBC 1234");
-        miModel.crearCoche("Alpine", "HYU 4567");
-        miModel.crearCoche("Aston Martin", "FGH 3333");
+        Model.crearCoche("LaFerrari", "SBC 1234");
+        Model.crearCoche("Alpine", "HYU 4567");
+        Model.crearCoche("Aston Martin", "FGH 3333");
 
 
         // modifica la velocidad
-        int nuevaVelocidad = miModel.cambiarVelocidad("SBC 1234", 30);
+        int nuevaVelocidad = Model.cambiarVelocidad("SBC 1234", 30);
 
         // recoje la velocidad y la muestra (tarea de la View)
-        boolean hecho = miView.muestraVelocidad("SBC 1234", miModel.getVelocidad("SBC 1234"));
+        boolean hecho = View.muestraVelocidad("SBC 1234", Model.getVelocidad("SBC 1234"));
 
         // Aplicar el aumento de velociadad
         int aumento = 20;
-        int velocidadAumentada = miModel.aumentarVelocidad("SBC 1234", aumento);
+        int velocidadAumentada = Model.aumentarVelocidad("SBC 1234", aumento);
         // Mostrar la velocidad aumentada
-        hecho = miView.muestraVelocidad("SBC 1234", velocidadAumentada);
-
+        hecho = View.muestraVelocidad("SBC 1234", Model.aumentarVelocidad( "SBC 1234", 20));
         // Mostrar la velocidad disminuida
-        hecho = miView.muestraVelocidad("SBC 1234", velocidadDisminuida);
-*/
+        hecho = View.muestraVelocidad("SBC 1234", Model.reducirVelocidad("SBC 1234", 10));
+
         //Mostrar si el coche tiene velocidad negativa
-        Model miModel = new Model();
-        Coche ferrari =        miModel.crearCoche( "Ferrari", "SBC 1234");
+
+        Coche ferrari =        Model.crearCoche( "Ferrari", "SBC 1234");
         // Cambiar la velocidad a 5
         ferrari.velocidad = 5;
-        int nuevaVelocidad = miModel.cambiarVelocidad("SBC 1234", 5);
+        Model.cambiarVelocidad("SBC 1234", 5);
         // hacer  la velocidad es negativa
         int reducir = 10;
         // reducir la velocidad
-        nuevaVelocidad= miModel.reducirVelocidad("SBC 1234", reducir);
+        nuevaVelocidad= Model.reducirVelocidad("SBC 1234", reducir);
         // si la velocidad es negativa
-        if (miModel.reducirVelocidad(ferrari.matricula,nuevaVelocidad)<0){
+        if (Model.reducirVelocidad(ferrari.matricula,nuevaVelocidad)<0){
             System.out.println(nuevaVelocidad);
         }else{
             System.out.println(nuevaVelocidad);
--- src/View.java
+++ src/View.java
@@ -8,8 +8,15 @@ public class View {
      * @param v velocidad
      * @return true si se ha mostrado correctamente
      */
-    public boolean muestraVelocidad(String matricula, Integer v){
+    public static boolean muestraVelocidad(String matricula, Integer v){
         System.out.println(matricula + ": " + v + "km/hr");
         return true;
+    }
+    public static void menu(){
+        System.out.println("1. Crear coche");
+        System.out.println("2. Aumentar velocidad");
+        System.out.println("3. Disminuir velocidad");
+        System.out.println("5. Mostrar velocidad");
+        System.out.println("6. Salir");
     }
 }
